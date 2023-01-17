1b810fa824fd097fd8725beca9555772ecda4505
YashuGv <venkatayasaswini66@gmail.com>
2023-01-17 07:15:12 +0530

    small changes in code

--- Project_1/Project_0/Console/GetAllTrainers.cs
+++ Project_1/Project_0/Console/GetAllTrainers.cs
@@ -38,37 +38,3 @@ namespace Console
         }
     }
 }
-/*namespace UI_Console
-{
-    public class TrainerMenu : IMenu
-    {
-        public void Display()
-        {
-            Console.WriteLine("[0] to Main Menu");
-            Console.WriteLine("[1] to Login");
-            Console.WriteLine("[2] to Signup");
-        }
-
-        public string UserChoice()
-        {
-            Console.Write("Enter your choice: ");
-            string userChoice = Console.ReadLine();
-
-            switch (userChoice)
-            {
-                case "0":
-                    return "MainMenu";
-                case "1":
-                    return "Login";
-                case "2":
-                    return "Signup"; 
-                default:
-                    Console.WriteLine("Wrong Choice! Try again...");
-                    Console.WriteLine("Enter to Continue...");
-                    Console.ReadLine();
-                    return "TrainerMenu";
-            }
-        }
-    }
-}
-*/
