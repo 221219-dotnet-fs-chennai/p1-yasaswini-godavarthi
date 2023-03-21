1aff80d0fbdbbc7c5ff38e0c5e1d5d692742ce20
YashuGv <venkatayasaswini66@gmail.com>
2023-03-21 16:38:18 +0530

    HMS project

--- capstone/Database_Tables.sql
+++ capstone/Database_Tables.sql
@@ -3,15 +3,21 @@ CREATE TABLE [PatientBasicRecord](
     [DateTime] smalldatetime,
     [Patient_Id] NVARCHAR(100) PRIMARY KEY,
     [Nurse_Id] NVARCHAR(max),
+    [Appointment_Id] NVARCHAR(max),
     [Bp] NVARCHAR(max),
     [Heart_Rate] int,
-    [SpO2] NVARCHAR(max) 
+    [SpO2] NVARCHAR(max),
+    [Weight] NVARCHAR(max),
+    [Height] NVARCHAR(max),
+    [Blood_Group] NVARCHAR(max),
+    [Temperature] NVARCHAR(max)
 );
 
 
 CREATE table [PatientAllergy](
     [Id] UNIQUEIDENTIFIER PRIMARY KEY,
     [Health_Id] NVARCHAR(100),
+    [Appointment_Id] NVARCHAR(max),
     [Allergy] NVARCHAR(max),
     CONSTRAINT [Fk_PatientAllergy]  FOREIGN Key(Health_Id) REFERENCES [PatientBasicRecord](Patient_Id) ON DELETE CASCADE ON UPDATE CASCADE
 )
@@ -22,12 +28,14 @@ CREATE TABLE [PatientHealthRecord](
     [DateTime] smalldatetime,
     [Patient_Id] NVARCHAR(max),
     [Doctor_Id] NVARCHAR(max),
+    [Appointment_Id] NVARCHAR(max),
     [Conclusion] NVARCHAR(max) 
 );
 
 Create TABLE [PatientMedication](
     [Id] UNIQUEIDENTIFIER PRIMARY Key,
     [Health_Id] UNIQUEIDENTIFIER,
+    [Appointment_Id] NVARCHAR(max),
     [Drug] NVARCHAR(max),
     CONSTRAINT [Fk_patientmedication] FOREIGN KEY(Health_Id) REFERENCES [PatientHealthRecord](Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -35,6 +43,7 @@ Create TABLE [PatientMedication](
 Create TABLE [PatientTest](
     [Id] UNIQUEIDENTIFIER PRIMARY Key,
     [Health_Id] UNIQUEIDENTIFIER,
+    [Appointment_Id] NVARCHAR(max),
     [Test] NVARCHAR(max),
     [Result] NVARCHAR(max),
     CONSTRAINT [Fk_patientTest] FOREIGN KEY(Health_Id) REFERENCES [PatientHealthRecord](Id) ON DELETE CASCADE ON UPDATE CASCADE
